82f59e6f93d95c8f97e6abbd5082dfb2587adef1
David Gomez <dg.dave@gmail.com>
2022-06-04 20:45:54 -0400

    Set up Tailwind CSS

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -2,7 +2,7 @@ import { Link } from "@remix-run/react";
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+    <div>
       <h1>Welcome to Baye</h1>
       <ul>
         <li>
