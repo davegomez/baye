f98c915c06bf7a2adbc51ec871592973c835dfce
David Gomez <dg.dave@gmail.com>
2022-06-06 19:35:28 -0400

    Create UI component Markdown

--- app/ui/markdown.tsx
+++ app/ui/markdown.tsx
@@ -1,11 +1,6 @@
 import * as React from 'react';
-import { remark } from 'remark';
-import SSBRef from 'ssb-ref';
-import { getFeedSSBURIRegex, getMessageSSBURIRegex } from 'ssb-uri2';
-import gemojiToEmoji from 'remark-gemoji-to-emoji';
-import linkifyRegex from 'remark-linkify-regex';
-import getUnicodeWordRegex from 'unicode-word-regex';
 import { twMerge } from 'tailwind-merge';
+import { processMarkdown } from '~/utils';
 
 type Props = {
   children: string;
@@ -14,33 +9,9 @@ type Props = {
 
 export type Ref = HTMLDivElement;
 
-/**
- * Match URIs *except* SSB URIs and File URIs
- */
-function getMiscURIRegex() {
-  return /\b((?=[a-z]+:)(?!(ssb:|file:)))[a-z]+:(\/\/)?[^ )\n]+/g;
-}
-
-const Button = React.forwardRef<Ref, Props>(
+const Markdown = React.forwardRef<Ref, Props>(
   ({ children, className }: Props, ref) => {
-    const linkifySsbSigilFeeds = linkifyRegex(SSBRef.feedIdRegex);
-    const linkifySsbSigilMsgs = linkifyRegex(SSBRef.msgIdRegex);
-    const linkifySsbUriFeeds = linkifyRegex(getFeedSSBURIRegex());
-    const linkifySsbUriMsgs = linkifyRegex(getMessageSSBURIRegex());
-    const linkifyMiscUris = linkifyRegex(getMiscURIRegex());
-    const linkifyHashtags = linkifyRegex(
-      new RegExp('#(' + getUnicodeWordRegex().source + '|\\d|-)+', 'gu')
-    );
-
-    const content = remark()
-      .use(gemojiToEmoji)
-      .use(linkifySsbSigilFeeds)
-      .use(linkifySsbSigilMsgs)
-      .use(linkifySsbUriFeeds)
-      .use(linkifySsbUriMsgs)
-      .use(linkifyMiscUris)
-      .use(linkifyHashtags)
-      .processSync(children);
+    const markdown = processMarkdown(children);
 
     return (
       <div
@@ -50,12 +21,12 @@ const Button = React.forwardRef<Ref, Props>(
         )}
         ref={ref}
       >
-        {String(content)}
+        {String(markdown)}
       </div>
     );
   }
 );
 
-Button.displayName = 'Button';
+Markdown.displayName = 'Markdown';
 
-export default Button;
+export default Markdown;
