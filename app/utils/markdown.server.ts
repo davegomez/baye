6a4bcea1bc31c54f21905fd0f3fca34f17a737f0
David Gomez <dg.dave@gmail.com>
2022-06-17 05:51:17 -0400

    Use dynamic imports

--- app/utils/markdown.server.ts
+++ app/utils/markdown.server.ts
@@ -1,30 +1,4 @@
-import {remark} from 'remark'
-import {visit} from 'unist-util-visit'
-import gemojiToEmoji from 'remark-gemoji-to-emoji'
-import linkifyRegex from 'remark-linkify-regex'
-import getUnicodeWordRegex from 'unicode-word-regex'
-import SSBRef from 'ssb-ref'
-import {getFeedSSBURIRegex, getMessageSSBURIRegex} from 'ssb-uri2'
 import toUrl from 'ssb-serve-blobs/id-to-url'
-
-const BLOB_REF = `&${Buffer.alloc(32).toString('base64')}.sha256`
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const imagesToSsbServeBlobs = () => (tree: any) => {
-  visit(tree, 'image', image => {
-    if (
-      image.url &&
-      typeof image.url === 'string' &&
-      SSBRef.isBlob(image.url.substr(0, BLOB_REF.length))
-    ) {
-      image.url = toUrl(image.url)
-    }
-
-    return image
-  })
-
-  return tree
-}
 
 /**
  * Match URIs *except* SSB URIs and File URIs
@@ -33,6 +7,14 @@ const getMiscURIRegex = () =>
   /\b((?=[a-z]+:)(?!(ssb:|file:)))[a-z]+:(\/\/)?[^ )\n]+/g
 
 export const preProcessMarkdown = async (rawMarkdown: string) => {
+  const {default: gemojiToEmoji} = await import('remark-gemoji-to-emoji')
+  const {default: getUnicodeWordRegex} = await import('unicode-word-regex')
+  const {default: linkifyRegex} = await import('remark-linkify-regex')
+  const {default: SSBRef} = await import('ssb-ref')
+  const {getFeedSSBURIRegex, getMessageSSBURIRegex} = await import('ssb-uri2')
+  const {remark} = await import('remark')
+  const {visit} = await import('unist-util-visit')
+
   const linkifySsbSigilFeeds = linkifyRegex(SSBRef.feedIdRegex)
   const linkifySsbSigilMsgs = linkifyRegex(SSBRef.msgIdRegex)
   const linkifySsbUriFeeds = linkifyRegex(getFeedSSBURIRegex())
@@ -41,6 +23,25 @@ export const preProcessMarkdown = async (rawMarkdown: string) => {
   const linkifyHashtags = linkifyRegex(
     new RegExp(`#(${getUnicodeWordRegex().source}|\\d|-)+`, 'gu'),
   )
+
+  const BLOB_REF = `&${Buffer.alloc(32).toString('base64')}.sha256`
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const imagesToSsbServeBlobs = () => (tree: any) => {
+    visit(tree, 'image', image => {
+      if (
+        image.url &&
+        typeof image.url === 'string' &&
+        SSBRef.isBlob(image.url.substr(0, BLOB_REF.length))
+      ) {
+        image.url = toUrl(image.url)
+      }
+
+      return image
+    })
+
+    return tree
+  }
 
   const markdown = await remark()
     .use(gemojiToEmoji)
@@ -54,4 +55,18 @@ export const preProcessMarkdown = async (rawMarkdown: string) => {
     .process(rawMarkdown)
 
   return markdown
+}
+
+export const compileMarkdown = async (processedMarkdown: string) => {
+  const {bundleMDX} = await import('mdx-bundler')
+
+  try {
+    const {code} = await bundleMDX({
+      source: processedMarkdown,
+    })
+
+    return {code}
+  } catch (e: unknown) {
+    throw new Error(`Markdown compilation error ${e}`)
+  }
 }
