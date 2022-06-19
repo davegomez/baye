cc4e1419aa0ae0da93325c2623fe0f42a365f589
David Gomez <dg.dave@gmail.com>
2022-06-18 21:06:57 -0400

    Move back to serverDependenciesToBundle

--- app/utils/markdown.server.ts
+++ app/utils/markdown.server.ts
@@ -1,4 +1,39 @@
+import { bundleMDX } from 'mdx-bundler'
+import { getFeedSSBURIRegex, getMessageSSBURIRegex } from 'ssb-uri2'
+import { remark } from 'remark'
+import { visit } from 'unist-util-visit'
+import gemojiToEmoji from 'remark-gemoji-to-emoji'
+import getUnicodeWordRegex from 'unicode-word-regex'
+import linkifyRegex from 'remark-linkify-regex'
+import path from 'path'
+import SSBRef from 'ssb-ref'
 import toUrl from 'ssb-serve-blobs/id-to-url'
+
+/**
+ * esbuild relies on __dirname to work out where is executable is and needs to
+ * be told manually where to look.
+ * Adding the following code before your bundleMDX will point esbuild directly
+ * at the correct executable for your platform.
+ * https://github.com/kentcdodds/mdx-bundler/blob/main/README.md#nextjs-esbuild-enoent
+ */
+if (process.platform === 'win32') {
+  process.env.ESBUILD_BINARY_PATH = path.join(
+    process.cwd(),
+    'node_modules',
+    'esbuild',
+    'esbuild.exe',
+  )
+} else {
+  process.env.ESBUILD_BINARY_PATH = path.join(
+    process.cwd(),
+    'node_modules',
+    'esbuild',
+    'bin',
+    'esbuild',
+  )
+}
+
+const BLOB_REF = `&${Buffer.alloc(32).toString('base64')}.sha256`
 
 /**
  * Match URIs *except* SSB URIs and File URIs
@@ -6,15 +41,24 @@ import toUrl from 'ssb-serve-blobs/id-to-url'
 const getMiscURIRegex = () =>
   /\b((?=[a-z]+:)(?!(ssb:|file:)))[a-z]+:(\/\/)?[^ )\n]+/g
 
-export const preProcessMarkdown = async (rawMarkdown: string) => {
-  const { default: gemojiToEmoji } = await import('remark-gemoji-to-emoji')
-  const { default: getUnicodeWordRegex } = await import('unicode-word-regex')
-  const { default: linkifyRegex } = await import('remark-linkify-regex')
-  const { default: SSBRef } = await import('ssb-ref')
-  const { getFeedSSBURIRegex, getMessageSSBURIRegex } = await import('ssb-uri2')
-  const { remark } = await import('remark')
-  const { visit } = await import('unist-util-visit')
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const imagesToSsbServeBlobs = () => (tree: any) => {
+  visit(tree, 'image', image => {
+    if (
+      image.url &&
+      typeof image.url === 'string' &&
+      SSBRef.isBlob(image.url.substr(0, BLOB_REF.length))
+    ) {
+      image.url = toUrl(image.url)
+    }
 
+    return image
+  })
+
+  return tree
+}
+
+export const preProcessMarkdown = async (rawMarkdown: string) => {
   const linkifySsbSigilFeeds = linkifyRegex(SSBRef.feedIdRegex)
   const linkifySsbSigilMsgs = linkifyRegex(SSBRef.msgIdRegex)
   const linkifySsbUriFeeds = linkifyRegex(getFeedSSBURIRegex())
@@ -23,25 +67,6 @@ export const preProcessMarkdown = async (rawMarkdown: string) => {
   const linkifyHashtags = linkifyRegex(
     new RegExp(`#(${getUnicodeWordRegex().source}|\\d|-)+`, 'gu'),
   )
-
-  const BLOB_REF = `&${Buffer.alloc(32).toString('base64')}.sha256`
-
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const imagesToSsbServeBlobs = () => (tree: any) => {
-    visit(tree, 'image', image => {
-      if (
-        image.url &&
-        typeof image.url === 'string' &&
-        SSBRef.isBlob(image.url.substr(0, BLOB_REF.length))
-      ) {
-        image.url = toUrl(image.url)
-      }
-
-      return image
-    })
-
-    return tree
-  }
 
   const markdown = await remark()
     .use(gemojiToEmoji)
@@ -58,8 +83,6 @@ export const preProcessMarkdown = async (rawMarkdown: string) => {
 }
 
 export const compileMarkdown = async (processedMarkdown: string) => {
-  const { bundleMDX } = await import('mdx-bundler')
-
   try {
     const { code } = await bundleMDX({
       source: processedMarkdown,
